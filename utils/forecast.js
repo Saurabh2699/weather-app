158f4814d84121ad96fa13d02dc380c3b79a0f0c
Saurabh2699 <44333869+Saurabh2699@users.noreply.github.com>
2019-12-13 19:10:40 +0530

    Some changes in forecast.js

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,21 +1,46 @@
-const request = require('request')
+const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-  const url = 'https://api.darksky.net/forecast/1fa5fbc0635e5f02b42de3d00cf62129/' + latitude + ',' + longitude + '?units=si'
+  const url =
+    "https://api.darksky.net/forecast/1fa5fbc0635e5f02b42de3d00cf62129/" +
+    latitude +
+    "," +
+    longitude +
+    "?units=si";
 
   request({ url: url, json: true }, (error, response) => {
-    const temp = response.body.currently.temperature
-    const rain = response.body.currently.precipProbability
-    const today = response.body.daily.data[0].summary
+    const temp = response.body.currently.temperature;
+    const rain = response.body.currently.precipProbability;
+    const today = response.body.daily.data[0].summary;
+    const high = response.body.daily.data[0].temperatureHigh;
+    const low = response.body.daily.data[0].temperatureLow;
 
     if (error) {
-      callback('Unable to connect to the weather service. Please check your internet connection...', undefined)
+      callback(
+        "Unable to connect to the weather service. Please check your internet connection...",
+        undefined
+      );
     } else if (response.body.error) {
-      callback('Cannot find the location. Check out the coordinates once again...', undefined)
+      callback(
+        "Cannot find the location. Check out the coordinates once again...",
+        undefined
+      );
     } else {
-      callback(undefined, `${today} There is currently ${temp} degrees out. There is ${rain}% chances of rain.`)
+      callback(
+        undefined,
+        today +
+          "\nThere is currently " +
+          temp +
+          " degrees out.\nThere is " +
+          rain +
+          "% chances of rain.\nHighest Temperature : " +
+          high +
+          " degrees\n and Lowest Temperature : " +
+          low +
+          " degrees."
+      );
     }
-  })
-}
+  });
+};
 
-module.exports = forecast
+module.exports = forecast;
